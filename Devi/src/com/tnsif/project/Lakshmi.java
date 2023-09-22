37c0e1c8e45a2857aea2374a5e5abb423d1080e4
madir <madir@10.0.0.133>
2023-09-22 14:41:44 +0530

    First commit

---
+++ Devi/src/com/tnsif/project/Lakshmi.java
@@ -0,0 +1,10 @@
+package com.tnsif.project;
+
+public class Lakshmi {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+ System.out.println("fly ever where you want");
+	}
+
+}
