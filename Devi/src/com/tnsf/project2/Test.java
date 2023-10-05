7bcd7efe9b47d645aba979bcc2e09fe455ab9820
madir <madir@Kirans__PC>
2023-10-05 14:21:38 +0530

    Fifth commit

---
+++ Devi/src/com/tnsf/project2/Test.java
@@ -0,0 +1,18 @@
+package com.tnsf.project2;
+
+public class Test {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+      int i=98;
+      double d=342.76;
+      char ch='s';
+      byte b=20;
+      short s=400;
+      long l=34809;
+      boolean x=true;
+      System.out.println("the value of the variable is  "  + ch+"  "+i );
+	}
+	}
+
+
